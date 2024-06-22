266f97c157089863545ae2e8c2c3fdbbcc47ddb0
rishabh709 <rishabhchaudhary760official@gmail.com>
2024-06-22 14:38:18 +0530

    udpate

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 
-import classes from "./LoginPage.module.css"
+import classes from "./SignupPage.module.css"
 
 function SignupPage() {
   return (
     <div className={classes.container}>
         <form action="">
-            <label htmlFor="email">Email</label>
+          <div className={classes.editText}>
+            <label htmlFor="email">Email </label>
             <input type="text" name="email" id="email" className={classes.inpt} />
+          </div>
+          <div className={classes.editText}>
             <label htmlFor="passwd">Password</label>
             <input type="password" name="passwd" id="passwd" className={classes.inpt} />
+          </div>
             <input type="submit" value="SignUp" className={classes.sbmt} />
         </form>
     </div>

7425b772ccc51b860db67812c22607ac12466622
Rishabh <rishabhchaudhary760official@gmail.com>
2025-08-14 13:19:11 +0530

    Re-designed Sign up Page

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,48 +1,85 @@
-import React, { useState } from 'react'
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
-import classes from "./SignupPage.module.css"
-import api from '../api/axiosInstance'
+import classes from "./SignupPage.module.css";
+import api from "../api/axiosInstance";
+import { motion } from "framer-motion";
 
 function SignupPage() {
-  const [newUserCred, setNewUserCred]= useState({name:"", email:"", passwd:""});
+  const [newUserCred, setNewUserCred] = useState({
+    name: "",
+    email: "",
+    passwd: "",
+  });
 
-  const submitHandler = async (event)=>{
+  const submitHandler = async (event) => {
     event.preventDefault();
-    name = newUserCred.name
+    name = newUserCred.name;
 
-    try{
-      const response = await api.post('/signup', {
+    try {
+      const response = await api.post("/signup", {
         player_name: newUserCred.name,
         email_id: newUserCred.email,
-        password: newUserCred.passwd
-      })
-      if(response.status == 200){
-        console.log("Created the user:::::")
+        password: newUserCred.passwd,
+      });
+      if (response.status == 200) {
+        console.log("Created the user:::::");
       }
-    } catch(error){
+    } catch (error) {
       console.log("Sign up Failed: ", error);
     }
     console.log("Submitted.....");
-  }
-  const hanleChange = (event)=>{
-    const {name, value} = event.target;
-    setNewUserCred(prev=>({...prev, [name]:value}))
-  }
+  };
+  const hanleChange = (event) => {
+    const { name, value } = event.target;
+    setNewUserCred((prev) => ({ ...prev, [name]: value }));
+  };
 
   return (
     <div className={classes.container}>
-        <form onSubmit={submitHandler}>
+      <form onSubmit={submitHandler}>
+        <div className={classes.title}>
           <h2>Sign-up</h2>
-          <input type="text" name="name" onChange={hanleChange} placeholder='Name' className={classes.inpt} required />
-          <input type="text" name="email" onChange={hanleChange} placeholder='Email' className={classes.inpt} required />
-          <input type="password" name="passwd" onChange={hanleChange} placeholder='Password' className={classes.inpt} required />
+        </div>
+        <div className={classes.body}>
+          <input
+            type="text"
+            name="name"
+            onChange={hanleChange}
+            placeholder="Name"
+            className={classes.inpt}
+            required
+          />
+          <input
+            type="text"
+            name="email"
+            onChange={hanleChange}
+            placeholder="Email"
+            className={classes.inpt}
+            required
+          />
+          <input
+            type="password"
+            name="passwd"
+            onChange={hanleChange}
+            placeholder="Password"
+            className={classes.inpt}
+            required
+          />
+        </div>
+        <div className={classes.bottom}>
           <div className={classes.actionBar}>
-            <input type="submit" value="SignUp" className={classes.sbmt} /> 
+            <motion.input
+            whileHover={{scale: 1.1,}}
+              type="submit"
+              value="SignUp"
+              className={classes.sbmt}
+            />
           </div>
-        </form>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default SignupPage
+export default SignupPage;

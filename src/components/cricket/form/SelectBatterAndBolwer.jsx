62ff69d657cfb61e43ad381fd7f40f5e2b60f09f
Rishabh <rishabhchaudhary760official@gmail.com>
2025-10-02 11:30:31 +0530

    refactor(SelectBatterAndBowler): replace old DropdownMenu with DropdownMenuV2
    - Swapped out legacy custom dropdown for the new DropdownMenuV2 component
    - Updated CSS and added wrapper divs to fix layout and styling issues
    - Ensured dropdown displays correctly and integrates seamlessly with existing UI

--- src/components/cricket/form/SelectBatterAndBolwer.jsx
+++ src/components/cricket/form/SelectBatterAndBolwer.jsx
@@ -4,6 +4,7 @@ import classes from "./SelectBatterAndBolwer.module.css";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 import DropdownMenu from "../../common/DropdownMenu";
 import { useMatchContext } from "../../../context/matchReducer";
+import DropdownMenuV2 from "../../common/DropdownMenuV2";
 
 function SelectBatterAndBolwer() {
   const [isDropDownOpen1, setIsDropDownOpen1] = useState(false);
@@ -23,49 +24,55 @@ function SelectBatterAndBolwer() {
   );
 
   const setOnStrikeBatter = (batterName) => {
-    matchDispatch({type:'SET_BATTER_ON_STRIKE', payload:batterName})
-  }
+    matchDispatch({ type: "SET_BATTER_ON_STRIKE", payload: batterName });
+  };
   const setOnNonStrikeBatter = (batterName) => {
-    matchDispatch({type:'SET_BATTER_ON_NONSTRIKE', payload:batterName})
-  }
+    matchDispatch({ type: "SET_BATTER_ON_NONSTRIKE", payload: batterName });
+  };
   const setBolwerName = (bolwerName) => {
-    matchDispatch({type:'SET_BOLWER_NAME', payload:bolwerName})
-  }
+    matchDispatch({ type: "SET_BOLWER_NAME", payload: bolwerName });
+  };
   return (
     <div
       style={{
         margin: "auto",
         width: "max-content",
+        minWidth: "60%",
         display: "flex",
         flexDirection: "column",
         gap: "10px",
       }}
     >
       <div className={classes.bar}>
-        <div>Batter on Strike</div>
-        <DropdownMenu
-          defaultValue={matchState.current_batter.onStrike}
-          setSelectedOption={setOnStrikeBatter}
-          options={matchState.players[matchState.battingTeam]}
-        ></DropdownMenu>
+        <div className={classes.lable}>Batter on Strike</div>
+        <div className={classes.optionsTag}>
+          <DropdownMenuV2
+            setSelectedOption={setOnStrikeBatter}
+            options={matchState.players[matchState.battingTeam]}
+          />
+        </div>
       </div>
 
       <div className={classes.bar}>
-        <div>Batter on Non Strike</div>
-        <DropdownMenu
-          defaultValue={matchState.current_batter.nonStrike}
-          setSelectedOption={setOnNonStrikeBatter}
-          options={matchState.players[matchState.battingTeam]}
-        ></DropdownMenu>
+        <div className={classes.lable}>Batter on Non Strike</div>
+        <div className={classes.optionsTag}>
+          <DropdownMenuV2
+            // defaultValue={matchState.current_batter.nonStrike}
+            setSelectedOption={setOnNonStrikeBatter}
+            options={matchState.players[matchState.battingTeam]}
+          ></DropdownMenuV2>
+        </div>
       </div>
-      <hr style={{borderTopColor:'blue'}} />
+      <hr style={{ borderTopColor: "blue" }} />
       <div className={classes.bar}>
-        <div>Bolwer</div>
-        <DropdownMenu
-          defaultValue={matchState.over.bowlerName}
-          setSelectedOption={setBolwerName}
-          options={matchState.players[matchState.bowlingTeam]}
-        ></DropdownMenu>
+        <div className={classes.lable}>Bolwer</div>
+        <div className={classes.optionsTag}>
+          <DropdownMenuV2
+            // defaultValue={matchState.over.bowlerName}
+            setSelected={setBolwerName}
+            options={matchState.players[matchState.bowlingTeam]}
+          ></DropdownMenuV2>
+        </div>
       </div>
     </div>
   );

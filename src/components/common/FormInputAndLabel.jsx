ca3b906d989edf8683a63d48bc4c0bc7baaead93
Rishabh <rishabhchaudhary760official@gmail.com>
2026-02-17 17:35:25 +0530

    add(FormInputAndLabel): Form for sidebar settings

---
+++ src/components/common/FormInputAndLabel.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormInput from './FormInput';
+
+function FormInputAndLabel() {
+    // inputField = inputField.inputField;
+  return (
+    <>
+    </>
+  )
+}
+
+export default FormInputAndLabel
